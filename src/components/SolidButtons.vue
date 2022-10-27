c3ff9402ef758e2aea82afa8a6b69e45ab6c14db
Phypyy <supanut_kus@hotmail.com>
2022-10-27 13:49:31 +0700

    Update tokens

--- src/components/SolidButtons.vue
+++ src/components/SolidButtons.vue
@@ -6,13 +6,13 @@
         class="inline-flex
         items-center
         rounded-md border
-        text-text-text-04
+        text-text-04
         bg-interactive-01
         px-4
         py-2
         text-sm
         font-medium
-        hover:bg-interactive-02">
+        hover:bg-hover-primary">
               Primary
             </button>
       </div>
@@ -21,13 +21,13 @@
         class="inline-flex
         items-center
         rounded-md border
-        text-text-text-04
+        text-text-04
         bg-interactive-02
         px-4
         py-2
         text-sm
         font-medium
-        hover:bg-hover-primary">
+        hover:bg-hover-secondary">
               Secondary
             </button>
       </div>
@@ -36,15 +36,15 @@
         class="inline-flex
         items-center
         rounded-md border 
-        text-text-text-02
-        border-ui-ui-03
-        bg-ui-ui-02
+        text-interactive-03
+        border-interactive-03
+        bg-ui-02
         px-4
         py-2
         text-sm
         font-medium
-        text-gray-700
-        hover:bg-hover-primary">
+        hover:bg-hover-tertiary
+        hover:text-inverse-01">
         tertiary
             </button>
       </div>
